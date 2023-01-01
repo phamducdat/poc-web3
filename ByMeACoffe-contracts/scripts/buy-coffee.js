187827d824fcb7fc86674bf9bc911adbd2fc2f88
datpd <phamducdat2402@gmail.com>
2023-01-02 03:22:34 +0700

    add: etherum

---
+++ ByMeACoffe-contracts/scripts/buy-coffee.js
@@ -0,0 +1,67 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+
+
+// Returns the Ether balance of a given address.
+async function getBalance(address) {
+  const balanceBigInt = await hre.ethers.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+// Logs the Ether balances for a list of addresses.
+async function printBalances(addresses) {
+  let idx = 0;
+  for (const address of addresses) {
+    console.log(`Address ${idx} balance: `, await getBalance(address));
+    idx++;
+  }
+}
+
+async function printMemos(memos) {
+  for (const memo of memos) {
+    const timestamp = memo.timestamp;
+    const tipper = memo.name;
+    const tipperAddress = memo.from;
+    const message = memo.message;
+    console.log(`At ${timestamp}, ${tipper} (${tipperAddress}) said: "${message}"`);
+  }
+}
+
+async function main() {
+  // Get the example accounts we'll be working with
+  const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
+
+  // We get the contract to deploy.
+  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+  const buyMeACoffee = await BuyMeACoffee.deploy();
+
+  // Deploy the contract.
+  await buyMeACoffee.deployed();
+  console.log("BuyMeACoffee deployed to: ", buyMeACoffee.address);
+
+  // Check balances before the coffee purchase.
+  const addresses = [owner.address, tipper.address, buyMeACoffee.address]
+  console.log("== start ==");
+  await printBalances(addresses);
+  // Buy the owner a few coffees.
+
+  // Check balances after the coffee purchase.
+
+  // Withdraw.
+
+  // Check balances after withdrawal.
+
+  // Check out the memos.
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
