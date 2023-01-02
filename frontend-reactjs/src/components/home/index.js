e389ac2cef5cd6b8350061e9e0e32ad99913a4a7
datpd <phamducdat2402@gmail.com>
2023-01-02 14:11:30 +0700

    buy coffee

--- frontend-reactjs/src/components/home/index.js
+++ frontend-reactjs/src/components/home/index.js
@@ -1,9 +1,13 @@
 import React, {useState} from "react";
 import abi from '../../utils/BuyMeACoffee.json';
-import {Button, Col, Form, Input, Row} from "antd";
+import {Button, Col, Form, Input, Row, Spin} from "antd";
+import {ethers} from "ethers";
 
 
 const Home = props => {
+
+    const [form] = Form.useForm()
+
     const contractAddress = "0x26B5FDF3d05D3F4c312dB1d2E85287aDe19F31a9";
     const contractABI = abi.abi;
 
@@ -11,6 +15,7 @@ const Home = props => {
     const [name, setName] = useState("")
     const [message, setMessage] = useState("")
     const [memos, setMemos] = useState("")
+    const [loading, setLoading] = useState(false)
 
 
     const connectWallet = async () => {
@@ -33,46 +38,93 @@ const Home = props => {
         }
     }
 
+    const buyCoffee = async (value) => {
+        try {
+            setLoading(true)
+            const {ethereum} = window;
+
+            if (ethereum) {
+                const provider = new ethers.providers.Web3Provider(ethereum, "any");
+                const signer = provider.getSigner();
+                const buyMeACoffee = new ethers.Contract(
+                    contractAddress,
+                    contractABI,
+                    signer
+                );
+
+                console.log("buying coffee..")
+                const coffeeTxn = await buyMeACoffee.buyCoffee(
+                    value.name, value.message,
+                    {value: ethers.utils.parseEther("0.001")}
+                );
+
+                await coffeeTxn.wait();
+
+                console.log("mined ", coffeeTxn.hash);
+
+                console.log("coffee purchased!")
+                form.resetFields()
+                setLoading(false)
+            }
+        } catch (err) {
+            console.log(err)
+            setLoading(false)
+        }
+    }
+
     return (
         <>
-            <p>Buy Datpd a Coffee!</p>
-            {currentAccount ?
-                <>
-                    <Form
-                        layout={"vertical"}
-                    >
-                        <Row justify={"center"}>
-                            <Col span={4}>
-                                <Form.Item
-                                    label={"Name"}
-                                    name={"name"}
-                                >
-                                    <Input/>
-                                </Form.Item>
-                            </Col>
-                        </Row>
-                        <Row justify={"center"}>
-                            <Col span={4}>
+                <p>Buy Datpd a Coffee!</p>
+                {currentAccount ?
+                    <>
+                        <Form
+                            layout={"vertical"}
+                            onFinish={buyCoffee}
+                            form={form}
+                        >
+                            <Row justify={"center"}>
+                                <Col span={4}>
+                                    <Form.Item
+                                        label={"Name"}
+                                        name={"name"}
+                                        initialValue={"anon"}
+                                    >
+                                        <Input/>
+                                    </Form.Item>
+                                </Col>
+                            </Row>
+                            <Row justify={"center"}>
+                                <Col span={4}>
 
-                                <Form.Item
-                                    label={"Send Datpd a message"}
-                                    name={"message"}
-                                >
-                                    <Input/>
-                                </Form.Item>
-                            </Col>
-                        </Row>
-                    </Form>
-                </>
+                                    <Form.Item
+                                        label={"Send Datpd a message"}
+                                        name={"message"}
+                                        initialValue={"Enjoy your coffee!"}
+                                    >
+                                        <Input/>
+                                    </Form.Item>
+                                </Col>
+                            </Row>
 
-                :
+                            <Row justify={"center"}>
+                                <Col>
+                                    <Button type={"primary"} htmlType="submit">
+                                        Send 1 Coffee for 0.001 ETH
+                                    </Button>
+                                </Col>
+
+                            </Row>
+                        </Form>
+                    </>
+
+                    :
 
 
-                <Button onClick={connectWallet}>
-                    Connect your wallet
-                </Button>
+                    <Button onClick={connectWallet}>
+                        Connect your wallet
+                    </Button>
 
-            }
+                }
         </>
     )
 };
