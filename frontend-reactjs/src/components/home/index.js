9d9718b89ab8c8b71fe5c6728004a07969794a47
datpd <phamducdat2402@gmail.com>
2023-01-02 15:18:16 +0700

    done

--- frontend-reactjs/src/components/home/index.js
+++ frontend-reactjs/src/components/home/index.js
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import abi from '../../utils/BuyMeACoffee.json';
-import {Button, Col, Form, Input, Row, Spin} from "antd";
+import {Button, Col, Form, Input, Row} from "antd";
 import {ethers} from "ethers";
+import {Footer} from "antd/es/layout/layout";
 
 
 const Home = props => {
@@ -16,6 +17,86 @@ const Home = props => {
     const [message, setMessage] = useState("")
     const [memos, setMemos] = useState("")
     const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        let buyMeACoffee;
+        isWalletConnected();
+        getMemos();
+
+        const onNewMemo = (from, timestamp, name, message) => {
+            console.log("Memo received: ", from, timestamp, name, message);
+            setMemos([
+                ...memos,
+                {
+                    address: from,
+                    timestamp: new Date(timestamp * 1000),
+                    message,
+                    name
+                }
+            ]);
+        };
+
+        const {ethereum} = window;
+
+        if (ethereum) {
+            const provider = new ethers.providers.Web3Provider(ethereum, "any");
+            const signer = provider.getSigner();
+            buyMeACoffee = new ethers.Contract(
+                contractAddress,
+                contractABI,
+                signer
+            );
+
+            buyMeACoffee.on("NewMemo", onNewMemo);
+        }
+        return () => {
+            if (buyMeACoffee) {
+                buyMeACoffee.off("NewMemo", onNewMemo);
+            }
+        }
+    }, [])
+
+    const isWalletConnected = async () => {
+        try {
+            const {ethereum} = window;
+
+            const accounts = await ethereum.request({method: 'eth_accounts'})
+            console.log("accounts: ", accounts);
+
+            if (accounts.length > 0) {
+                const account = accounts[0];
+                console.log("wallet is connected! " + account);
+            } else {
+                console.log("make sure MetaMask is connected")
+            }
+        } catch (error) {
+            console.log("error: ", error)
+        }
+    }
+
+    const getMemos = async () => {
+        try {
+            const {ethereum} = window;
+            if (ethereum) {
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const signer = provider.getSigner();
+                const buyMeACoffee = new ethers.Contract(
+                    contractAddress,
+                    contractABI,
+                    signer
+                );
+
+                console.log("fetching memos from the blockchain...");
+                const memos = await buyMeACoffee.getMemos();
+                console.log("fetched!")
+                setMemos(memos);
+            } else {
+                console.log("Metamask is not connected");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
 
     const connectWallet = async () => {
@@ -74,57 +155,73 @@ const Home = props => {
 
     return (
         <>
-                <p>Buy Datpd a Coffee!</p>
-                {currentAccount ?
-                    <>
-                        <Form
-                            layout={"vertical"}
-                            onFinish={buyCoffee}
-                            form={form}
-                        >
-                            <Row justify={"center"}>
-                                <Col span={4}>
-                                    <Form.Item
-                                        label={"Name"}
-                                        name={"name"}
-                                        initialValue={"anon"}
-                                    >
-                                        <Input/>
-                                    </Form.Item>
-                                </Col>
-                            </Row>
-                            <Row justify={"center"}>
-                                <Col span={4}>
+            <p>Buy Datpd a Coffee!</p>
+            {currentAccount ?
+                <>
+                    <Form
+                        layout={"vertical"}
+                        onFinish={buyCoffee}
+                        form={form}
+                    >
+                        <Row justify={"center"}>
+                            <Col span={4}>
+                                <Form.Item
+                                    label={"Name"}
+                                    name={"name"}
+                                    initialValue={"anon"}
+                                >
+                                    <Input/>
+                                </Form.Item>
+                            </Col>
+                        </Row>
+                        <Row justify={"center"}>
+                            <Col span={4}>
 
-                                    <Form.Item
-                                        label={"Send Datpd a message"}
-                                        name={"message"}
-                                        initialValue={"Enjoy your coffee!"}
-                                    >
-                                        <Input/>
-                                    </Form.Item>
-                                </Col>
-                            </Row>
+                                <Form.Item
+                                    label={"Send Datpd a message"}
+                                    name={"message"}
+                                    initialValue={"Enjoy your coffee!"}
+                                >
+                                    <Input/>
+                                </Form.Item>
+                            </Col>
+                        </Row>
 
-                            <Row justify={"center"}>
-                                <Col>
-                                    <Button type={"primary"} htmlType="submit">
-                                        Send 1 Coffee for 0.001 ETH
-                                    </Button>
-                                </Col>
+                        <Row justify={"center"}>
+                            <Col>
+                                <Button type={"primary"} htmlType="submit">
+                                    Send 1 Coffee for 0.001 ETH
+                                </Button>
+                            </Col>
 
-                            </Row>
-                        </Form>
-                    </>
+                        </Row>
+                    </Form>
+                </>
 
-                    :
+                :
 
 
-                    <Button onClick={connectWallet}>
-                        Connect your wallet
-                    </Button>
+                <Button onClick={connectWallet}>
+                    Connect your wallet
+                </Button>
 
-                }
+            }
+
+            {currentAccount &&
+                <Footer style={{marginTop: "160px"}}>
+                    <p>Memos received</p>
+                </Footer>
+            }
+            {
+                currentAccount && (memos.map((memo, idx) => {
+                    return (
+                        <>
+                            <p>From: {memo.name} at {memo.timestamp.toString()}</p>
+
+                        </>
+                    )
+                }))
+            }
         </>
     )
 };
