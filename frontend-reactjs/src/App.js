30254b0e7899bb18ccee9f2f0d22b6064cd46e2a
datpd <phamducdat2402@gmail.com>
2023-01-02 13:45:15 +0700

    connect your wallet

--- frontend-reactjs/src/App.js
+++ frontend-reactjs/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Home from "./components/home";
+import {Content} from "antd/es/layout/layout";
 
 const App = props => {
     return (
-        <div>
-            <Home/>
+        <div style={{alignItems: 'center', textAlign: 'center'}}>
+                <Home/>
         </div>
     );
 };
