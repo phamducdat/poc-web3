9b4a63360232f7451f23c87f2a53099da8cf60fa
datpd <phamducdat2402@gmail.com>
2023-01-04 07:29:59 +0700

    update

--- BuyMeACoffee-contracts/scripts/staking.js
+++ BuyMeACoffee-contracts/scripts/staking.js
@@ -2,6 +2,17 @@ const hre = require("hardhat");
 
 // Returns the Ether balance of a given address.
 
+async function getBalance(address) {
+    const balanceBigInt = await hre.ethers.provider.getBalance(address);
+    return hre.ethers.utils.formatEther(balanceBigInt);
+}
+async function printBalances(addresses) {
+    let idx = 0;
+    for (const address of addresses) {
+        console.log(`Address ${idx} balance: `, await getBalance(address));
+        idx ++;
+    }
+}
 async function main() {
     // Get the example accounts we'll be working with.
     const [owner, tipper, tipper2, tipper3] = await hre.ethers.getSigners();
@@ -13,6 +24,9 @@ async function main() {
     await staking.deployed()
     console.log("Staking deployed to: ", staking.address)
 
+    const addresses = [owner.address, tipper.address, staking.address];
+    console.log("== start ==");
+    await printBalances(addresses);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
