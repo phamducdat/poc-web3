29064f655bb9f858d39c5b06b46459d29ee85a5f
datpd <phamducdat2402@gmail.com>
2023-01-10 23:30:00 +0700

    update

--- blockchain/contracts/Staking.sol
+++ blockchain/contracts/Staking.sol
@@ -8,22 +8,72 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract Staking {
 
     IERC20 public s_stakingToken;
+    IERC20 public s_rewardToken;
     // someone address -> how much they staked
     mapping(address => uint256) public s_balances;
 
+    // a mapping of how much each address has been paid
+    mapping(address => uint256) public s_userRewardPerTokenPaid;
+    mapping(address => uint256) public s_rewards;
+
+    uint256 public constant REWARD_RATE = 100;
     uint256 public s_totalSupply;
+    uint256 public s_rewardPerTokenStored;
+    uint256 public s_lastUpdateTime;
+
+    modifier updateReward(address account) {
+        // how much reward per token?
+        // last timestamp
+        // 12 - 1, user
+
+        s_rewardPerTokenStored = rewardPerToken();
+        s_lastUpdateTime = block.timestamp;
+        s_rewards[account] = earned(account);
+        s_userRewardPerTokenPaid[account] = s_rewardPerTokenStored;
+        _;
+    }
+
+    function earned(address account) public view returns (uint256) {
+        uint256 currentBalance = s_balances[account];
+        // how much they have been paid already
+        uint256 amountPaid = s_userRewardPerTokenPaid[account];
+
+        uint256 currentRewardPerToken = rewardPerToken();
+
+        uint256 pastRewards = s_rewards[account];
+
+        uint256 earned = ((currentBalance * (currentRewardPerToken - amountPaid))
+        / 1e18) + pastRewards;
+
+        return earned;
+
+    }
+
+
+    function rewardPerToken() public view returns (uint256)  {
+        if (s_totalSupply == 0) {
+            return s_rewardPerTokenStored;
+        }
+        return s_rewardPerTokenStored +
+        (((block.timestamp - s_lastUpdateTime) * REWARD_RATE * 1e18)
+        / s_totalSupply);
+    }
 
 
 
-    constructor(address stakingToken) {
+
+
+
+    constructor(address stakingToken, address rewardToken) {
         s_stakingToken = IERC20(stakingToken);
+        s_rewardToken = IERC20(rewardToken);
 
 
     }
 
     // do we allow any tokens?
     // or just a specific token?
-    function stake(uint256 amount) external {
+    function stake(uint256 amount) external updateReward(msg.sender) {
         // keep track of how much this user has staked
         // keep track of how much token we have total
         // transfer the token this contract
@@ -37,11 +87,26 @@ contract Staking {
         }
     }
 
-    function withdraw(uint256 amount) external  {
+    function withdraw(uint256 amount) external updateReward(msg.sender) {
         s_balances[msg.sender] = s_balances[msg.sender] - amount;
         s_totalSupply = s_totalSupply - amount;
         bool success = s_stakingToken.transfer(msg.sender, amount);
 
-        
+        if (!success) {
+            revert Staking__TransferFailed();
+        }
     }
+
+    function claimReward() external updateReward(msg.sender) {
+        uint256 reward = s_rewards[msg.sender];
+        bool success = s_rewardToken.transfer(msg.sender, reward);
+
+        if (!success) {
+            revert Staking__TransferFailed();
+        }
+
+
+    }
+
+
 }
