965dd797c623942e1cd9f11f077fb152a762f677
datpd <phamducdat2402@gmail.com>
2023-01-11 00:05:10 +0700

    add: deploy

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-deploy")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  nameAccounts: {
+    deployer: {
+      default: 0, // ethers built in accounts at index 0
+    }
+  }
 };
---
+++ blockchain/scripts/00-deploy-reward-token.js
@@ -0,0 +1,12 @@
+module.exports = async ({getNameAccounts, deployments}) => {
+    const {deploy} = deployments
+    const deployer = await getNameAccounts()
+
+    const rewardToken = await deploy("RewardToken", {
+        from: deployer,
+        args: [],
+        log: true,
+    })
+}
+
+module.exports.tags = ["all", "rewardToken"]
