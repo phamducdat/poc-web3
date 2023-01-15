ba712acb930a24154371529b8007a51456560c93
datpd <phamducdat2402@gmail.com>
2023-01-15 18:16:12 +0700

    done: increment coin when stake

--- blockchain/scripts/my-staking.js
+++ blockchain/scripts/my-staking.js
@@ -1,13 +1,35 @@
 const {ethers} = require("hardhat");
+const hre = require("hardhat");
+const {moveTime} = require("../utils/move-time");
 
+async function getBalance(address) {
+    const balanceBigInt = await hre.ethers.provider.getBalance(address);
+    return hre.ethers.utils.formatEther(balanceBigInt);
+}
 
 async function main() {
-   const [signer1, signer2] = await ethers.getSigners();
-   const Staking = await ethers.getContractFactory("MyStaking", signer1);
+    const [owner, account] = await ethers.getSigners();
+    const Staking = await ethers.getContractFactory("MyStaking", owner);
 
-   const staking = await Staking.deploy({value: ethers.utils.parseEther('10')});
-   await staking.deployed();
-    console.log("dat with staking = ", await staking.owner());
+    const staking = await Staking.deploy({value: ethers.utils.parseEther('200')});
+    await staking.deployed();
+
+    console.log("Balance of account before = ", await getBalance(account.address));
+    console.log("===========================")
+    const totalStakeEther = ethers.utils.parseEther('100');
+    await staking.connect(account).stakeEther(180, {value:totalStakeEther});
+    console.log("Balance of account after = ", await getBalance(account.address))
+    // console.log("Position before 30 day = ", await staking.positions(0)) ;
+
+    console.log("===========================")
+    await moveTime(17280001)
+    console.log("===========================")
+    await staking.connect(account).closePosition(0);
+    // console.log("Position after 30 day = ", await staking.positions(0)) ;
+    console.log("Balance of account after 200 days = ", await getBalance(account.address))
+
+    
+    
 }
 
 
