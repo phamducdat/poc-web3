42dc421a6a896319737d3aa21fa5d9d7d13e9d15
datpd <phamducdat2402@gmail.com>
2023-01-26 15:11:06 +0700

    add: my-staking-client

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -3,6 +3,6 @@ require("@nomiclabs/hardhat-waffle");
 module.exports = {
   solidity: "0.8.17",
   paths: {
-    artifacts: "./client/src/artifacts",
+    artifacts: "./my-staking-client/src/artifacts",
   }
 };
