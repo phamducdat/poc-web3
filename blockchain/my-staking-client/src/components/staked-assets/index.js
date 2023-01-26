e0d6b1c0c89b20e8f9a6bb6c7bf6b027d8ba47f5
datpd <phamducdat2402@gmail.com>
2023-01-26 18:47:38 +0700

    update

--- blockchain/my-staking-client/src/components/staked-assets/index.js
+++ blockchain/my-staking-client/src/components/staked-assets/index.js
@@ -1,16 +1,54 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import {Table} from "antd";
+import {displayLogo} from "../../utils";
 
 const StakedAssets = props => {
+
+    const columns = [
+        {
+            title: 'Address',
+            dataIndex: 'address',
+            key: 'address',
+        },
+        {
+            title: "Asset",
+            dataIndex: "asset",
+            key: 'asset',
+            render: (text, record) => {
+                return displayLogo(record.symbol)
+            }
+        },
+        {
+            title: "Tokens Staked",
+            dataIndex: "tokenStaked",
+            key: "tokenStaked",
+        },
+        {
+            title: "Market Value (USD)",
+            dataIndex: "marketValue",
+            key: "marketValue",
+        },
+        {
+            title: "Accrued Interest (USD)",
+            dataIndex: "accruedInterest",
+            key: "accruedInterest",
+        },
+        {
+            title: "Accrued Interest (ETH)",
+            dataIndex: "accruedInterest",
+            key: "accruedInterest"
+        }
+    ]
+
     return (
-        <div>
-            StakedAssets
-        </div>
+        <>
+            <Table
+                columns={columns}
+            />
+        </>
     );
 };
 
-StakedAssets.propTypes = {
-
-};
+StakedAssets.propTypes = {};
 
 export default StakedAssets;
